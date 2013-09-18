8ec1f8be2fc4f45b07f054d9097b9ba589a78c8a
Stefan Bechtold <stefan.bechtold@me.com>
2013-09-18 12:58:56 +0200

    Standalone implementation of the hierarchical context runner

---
+++ src/main/java/org/junit/experimental/runners/context/ContextHierarchyLevelClassRunner.java
@@ -0,0 +1,133 @@
+package org.junit.experimental.runners.context;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.internal.runners.statements.RunAfters;
+import org.junit.internal.runners.statements.RunBefores;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.InitializationError;
+import org.junit.runners.model.Statement;
+import org.junit.runners.model.TestClass;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+class ContextHierarchyLevelClassRunner extends BlockJUnit4ClassRunner {
+    private LinkedList<Object> instances = null;
+
+    public ContextHierarchyLevelClassRunner(final Class<?> tClass) throws InitializationError {
+        super(tClass);
+    }
+
+    @Override
+    protected String getName() {
+        return getTestClass().getJavaClass().getSimpleName();
+    }
+
+    @Override
+    protected void validateNoNonStaticInnerClass(final List<Throwable> errors) {
+        // This contradicts the concept of the hierarchical context runner!
+    }
+
+    @Override
+    protected void validateConstructor(final List<Throwable> errors) {
+        if (!getTestClass().getJavaClass().isMemberClass()) {
+            super.validateConstructor(errors);
+        } else {
+            validateContextConstructor(errors);
+        }
+    }
+
+    private void validateContextConstructor(final List<Throwable> errors) {
+        final Class<?> enclosingClass = getTestClass().getJavaClass().getEnclosingClass();
+        final Class<?>[] parameterTypes = getTestClass().getOnlyConstructor().getParameterTypes();
+
+        if (parameterTypes.length != 1) {
+            String gripe = "Test context should have exactly one public one-argument constructor!";
+            errors.add(new Exception(gripe));
+        } else if (enclosingClass != null && !enclosingClass.isAssignableFrom(parameterTypes[0])) {
+            String gripe = "Context constructor must take an instance of the enclosing class!";
+            errors.add(new Exception(gripe));
+        }
+    }
+
+    @Override
+    protected Statement classBlock(final RunNotifier notifier) {
+        // Top level class has already been processed by the
+        // HierarchicalContextRunner, so just invoke children!
+        return childrenInvoker(notifier);
+    }
+
+    @Override
+    protected Statement withBefores(final FrameworkMethod method, final Object target, final Statement next) {
+        Statement statement = next;
+        for (int i = instances.size() - 1; i >= 0; i--) {
+            final Object instance = instances.get(i);
+            final TestClass tClass = new TestClass(instance.getClass());
+            final List<FrameworkMethod> befores = tClass.getAnnotatedMethods(Before.class);
+            statement = befores.isEmpty() ? statement : new RunBefores(statement, befores, instance);
+        }
+        return statement;
+    }
+
+    @Override
+    protected Statement withAfters(final FrameworkMethod method, final Object target, final Statement next) {
+        Statement statement = next;
+        for (int i = instances.size() - 1; i >= 0; i--) {
+            final Object instance = instances.get(i);
+            final TestClass tClass = new TestClass(instance.getClass());
+            final List<FrameworkMethod> afters = tClass.getAnnotatedMethods(After.class);
+            statement = afters.isEmpty() ? statement : new RunAfters(statement, afters, instance);
+        }
+        return statement;
+    }
+
+    @Override
+    protected Object createTest() throws Exception {
+        ensureHierarchicalFixturesAreValid();
+        return instances.getLast();
+    }
+
+    /**
+     * Returns new fixtures for the entire class hierarchy for running a test.
+     * Default implementation executes the test class's no-argument constructor
+     * and the inner class default constructor taking the outer class' instance
+     * (validation should have ensured one exists).
+     */
+    protected synchronized void ensureHierarchicalFixturesAreValid() throws Exception {
+        if (instances == null) {
+            final Stack<Class<?>> classHierarchy = getClassHierarchy();
+            instances = new LinkedList<Object>();
+
+            // Top level class has empty constructor
+            instances.add(classHierarchy.pop().newInstance());
+
+            // Inner class constructors require the enclosing instance
+            while (!classHierarchy.empty()) {
+                final Object enclosingInstance = instances.getLast();
+                final Class<?> innerClass = classHierarchy.pop();
+                instances.add(createInnerInstance(enclosingInstance, innerClass));
+            }
+        }
+    }
+
+    private Stack<Class<?>> getClassHierarchy() {
+        final Stack<Class<?>> classHierarchy = new Stack<Class<?>>();
+        for (Class<?> currentClass = getTestClass().getJavaClass();
+                currentClass != null;
+                currentClass = currentClass.getEnclosingClass())
+            classHierarchy.push(currentClass);
+        return classHierarchy;
+    }
+
+    private Object createInnerInstance(Object outerInstance, Class<?> innerClass) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        final Class<?> outerClass = outerInstance.getClass();
+        final Constructor<?> innerConstructor = innerClass.getConstructor(outerClass);
+        return innerConstructor.newInstance(outerInstance);
+    }
+}
---
+++ src/main/java/org/junit/experimental/runners/context/HierarchicalContextRunner.java
@@ -0,0 +1,115 @@
+package org.junit.experimental.runners.context;
+
+import org.junit.Test;
+import org.junit.internal.runners.statements.Fail;
+import org.junit.runner.Description;
+import org.junit.runner.Runner;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.ParentRunner;
+import org.junit.runners.model.InitializationError;
+import org.junit.runners.model.Statement;
+import org.junit.runners.model.TestClass;
+
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HierarchicalContextRunner extends ParentRunner<Class<?>> {
+    public HierarchicalContextRunner(final Class<?> testClass) throws InitializationError {
+        super(testClass);
+    }
+
+    @Override
+    protected List<Class<?>> getChildren() {
+        final Class<?>[] declaredClasses = getTestClass().getJavaClass().getDeclaredClasses();
+        final List<Class<?>> children = new ArrayList<Class<?>>(declaredClasses.length);
+
+        for (final Class<?> child : declaredClasses)
+            if (!Modifier.isStatic(child.getModifiers()))
+                children.add(child);
+
+        return children;
+    }
+
+    @Override
+    protected Description describeChild(final Class<?> tClass) {
+        try {
+            // Retrieve the description from the runner
+            return new HierarchicalContextRunner(tClass).getDescription();
+        } catch (InitializationError e) {
+            return Description.createTestDescription(tClass, tClass.getName(), tClass.getAnnotations());
+        }
+    }
+
+    @Override
+    protected void runChild(final Class<?> tClass, final RunNotifier runNotifier) {
+        try {
+            new HierarchicalContextRunner(tClass).run(runNotifier);
+        } catch (InitializationError e) {
+            runNotifier.fireTestFailure(new Failure(getDescription(), e));
+        }
+    }
+
+    @Override
+    protected String getName() {
+        return getTestClass().getJavaClass().getSimpleName();
+    }
+
+    @Override
+    public Description getDescription() {
+        Runner runner;
+        try {
+            runner = getRunnerForContextHierarchyLevel(getTestClass());
+        } catch (InitializationError e) {
+            runner = null;
+        }
+
+        final Description description = (runner != null)
+                ? runner.getDescription()
+                : Description.createSuiteDescription(getName(), getRunnerAnnotations());
+        for (Class<?> child : getChildren())
+            description.addChild(describeChild(child));
+        return description;
+    }
+
+    @Override
+    protected Statement childrenInvoker(final RunNotifier notifier) {
+        // Wrap children statement with tests for current hierarchy
+        final Statement next = super.childrenInvoker(notifier);
+        return withTestsForCurrentContextHierarchyLevel(next, notifier);
+    }
+
+    private Statement withTestsForCurrentContextHierarchyLevel(final Statement next, final RunNotifier notifier) {
+        try {
+            final Runner runner = getRunnerForContextHierarchyLevel(getTestClass());
+            return new Statement() {
+                @Override
+                public void evaluate() throws Throwable {
+                    if (runner != null)
+                        runner.run(notifier);
+                    next.evaluate();
+                }
+            };
+        } catch (InitializationError e) {
+            return new Fail(e);
+        }
+    }
+
+    /**
+     * Returns a {@link Runner} for evaluating the tests of the given context
+     * class within the hierarchy level. The current implementation uses the
+     * {@link ContextHierarchyLevelClassRunner}. This method may return null,
+     * if there are no tests to run within the context hierarchy level.
+     *
+     * @param tClass the {@link TestClass} under test
+     * @return a {@link Runner} instance or null
+     * @throws InitializationError if the runner cannot be initialized
+     */
+    protected Runner getRunnerForContextHierarchyLevel(final TestClass tClass) throws InitializationError {
+        if (!tClass.getAnnotatedMethods(Test.class).isEmpty())
+            return new ContextHierarchyLevelClassRunner(tClass.getJavaClass());
+        else
+            return null;
+    }
+}
